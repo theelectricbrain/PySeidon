c31a6bf21ca60665907626cb030000cc70d6296e
Grumpynounours <thomas.roc@gmail.com>
2014-08-14 18:34:19 -0300

    Fixed setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-
 def readme():
     with open('README.md') as f:
         return f.read()
@@ -15,4 +14,11 @@ setup(name='PySeidon',
       maintainer='Thomas Roc',
       license='GNU Affero GPL v3.0',
       packages=['PySeidon'],
+      package_dir={'PySeidon': 'PySeidon/fvcomClass'},
+      package_dir={'PySeidon': 'PySeidon/adcpClass'},
+      package_dir={'PySeidon': 'PySeidon/drifterClass'},
+      package_dir={'PySeidon': 'PySeidon/stationClass'},
+      package_dir={'PySeidon': 'PySeidon/tidegaugeClass'},
+      package_dir={'PySeidon': 'PySeidon/validationClass'},
+      package_dir={'PySeidon': 'PySeidon/utilities'},
       zip_safe=False)
