c7c60859b8a2e556f2872df78c5f72938bd90918
Grumpynounours <thomas.roc@gmail.com>
2014-08-14 21:18:43 -0300

    Fixed setup file

---
+++ PySeidon/tidegaugeClass/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2.7
+# encoding: utf-8
+
+from __future__ import division
+import os
+import sys
+
+__all__ = ["TIDEGAUGE"]
+#Local import
+from tidegaugeClass import TIDEGAUGE
+
+__version__ = '0.0'
+__authors__ = ['Wesley Bowman, Thomas Roc, Jonathan Smith']
+__licence__ = 'GNU Affero GPL v3.0'
+__copyright__ = 'Copyright (c) 2014 EcoEnergyII'
+
